f85cbd2f39020b7e240525edbdf1f31076f1085f
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2025-07-08 17:26:54 +0200

    Initial commit

---
+++ src/main/java/pizzamafia/CMbackend/CMbackendApplication.java
@@ -0,0 +1,14 @@
+package pizzamafia.CMbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CMbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CMbackendApplication.class, args);
+		System.out.println("ciao");
+	}
+
+}
---
+++ src/test/java/pizzamafia/CMbackend/CMbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pizzamafia.CMbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CMbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
