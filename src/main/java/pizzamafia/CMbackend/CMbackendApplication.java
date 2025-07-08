ba798dac549983c66ff180c9ba628df95f5de7a6
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2025-07-08 17:38:53 +0200

    Start
    Add Dependencies

--- src/main/java/pizzamafia/CMbackend/CMbackendApplication.java
+++ src/main/java/pizzamafia/CMbackend/CMbackendApplication.java
@@ -2,13 +2,16 @@ package pizzamafia.CMbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class CMbackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CMbackendApplication.class, args);
-		System.out.println("ciao");
-	}
+		var context = SpringApplication.run(CMbackendApplication.class, args);
+		ConfigurableEnvironment env = context.getEnvironment();
 
+		String jwtSecret = env.getProperty("jwt.secret");
+		System.out.println("JWT SECRET LETTO: " + jwtSecret);
+	}
 }
